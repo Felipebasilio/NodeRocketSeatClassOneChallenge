9c89bcddc2abdb38a8459992157dba41f46f5840
Felipe Basilio <fcb.97@hotmail.com>
2025-04-05 22:33:41 -0300

    Last commit: finished challenge requirements

--- streams/buffer.js
+++
@@ -1,4 +0,0 @@
-const buf = Buffer.from("hello")
-
-console.log(buf.toJSON())
-// Buffer: ver performatic way to store and access data
---
+++ streams/import-tasks.csv.js
@@ -0,0 +1,34 @@
+import { parse } from 'csv-parse';
+import fs from 'node:fs';
+
+const csvPath = new URL('./tasks.csv', import.meta.url);
+
+const stream = fs.createReadStream(csvPath);
+
+const csvParse = parse({
+  delimiter: ',',
+  skipEmptyLines: true,
+  fromLine: 2
+});
+
+async function run() {
+  const rowsParse = stream.pipe(csvParse);
+
+  for await (const row of rowsParse) {
+    const [title, description] = row;
+
+    await fetch('http://localhost:3333/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title,
+        description,
+      })
+    })
+  }
+
+}
+
+run()
